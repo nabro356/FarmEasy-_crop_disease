9ca95f6758e0a7a295348b9c4b0cb9337b422e18
Eshwar Vijay Dheeraj K <112922661+nabro356@users.noreply.github.com>
2024-03-02 04:26:32 +0530

    Update page.tsx

--- web/src/app/home/page.tsx
+++ web/src/app/home/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center px-5 py-24 md:p-24 overflow-x-hidden">
       <div className="text-center mb-auto">
-        <h1 className="scroll-m-20 mb-4 text-3xl md:text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1 bg-color='green'> FarmEasy</h1>
           <span className="gradient-text">{siteConfig.name}</span> - Plant
           Disease Detection
         </h1>
