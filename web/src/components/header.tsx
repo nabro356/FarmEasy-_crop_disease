ac757f66fc7c51ecd58b755e815ada2a97054306
Eshwar Vijay Dheeraj K <112922661+nabro356@users.noreply.github.com>
2024-03-02 04:14:38 +0530

    Update header.tsx

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -4,8 +4,5 @@ interface HeaderProps {
 
 export default function Header({ heading }: HeaderProps) {
   return (
-    <p className="scroll-m-20 px-4 text-center text-3xl md:text-5xl font-semibold tracking-tight transition-colors first:mt-0 gradient-text">
-      FarmEasy
-    </p>
-  )
+    <p>FarmEasy </p>  )
 }
